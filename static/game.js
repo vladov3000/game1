fc7bdb54307848f3a6117487117c51b3f2f91328
Vlad <vladov3000@gmail.com>
2018-07-18 08:23:32 -0700

    3

--- static/game.js
+++ static/game.js
@@ -1,74 +1,22 @@
 var socket = io();
+
+var sendButton = document.getElementById("sendButton");
+var chat = document.getElementById("chat")
+var chatInputArea = document.getElementById("chatInputArea");
+var chatInput = document.getElementById("chatInput");
+var canvas = document.getElementById('canvas');
+var scale = canvas.width/800;
+var messages = document.getElementById("messages");
+var usernameInput = document.getElementById("usernameInput")
+
 var movement = {
   up: false,
   down: false,
   left: false,
   right: false
 }
-document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
-    case 87: // W
-      movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
-      break;
-    case 83: // S
-      movement.down = true;
-      break;
-  }
-});
-document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = false;
-      break;
-    case 87: // W
-      movement.up = false;
-      break;
-    case 68: // D
-      movement.right = false;
-      break;
-    case 83: // S
-      movement.down = false;
-      break;
-  }
-});
 
-socket.emit('new player');
-setInterval(function() {
-  socket.emit('movement', movement);
-}, 1000 / 60);
-
-var sendButton = document.getElementById("sendButton");
-var chatInputArea = document.getElementById("chatInputArea");
-var chatInput = document.getElementById("chatInput");
-var canvas = document.getElementById('canvas');
-var scale = canvas.width/800;
-var messages = document.getElementById("messages");
-
-function sendMessage(){
-	socket.emit('sendMessage',chatInput.value);
-	chatInput.value = "";
-}
-
-chatInput.onkeydown = function(event) {
-    if (event.keyCode == 13) {
-        sendMessage();
-    }
-}
-
-socket.on ('loadMessage',function(data){
-	console.log(data)
-	var para = document.createElement("p");
-	para.className = "message";
-	var text = document.createTextNode(data);
-	para.appendChild(text);
-	messages.prepend(para);
-});
+chat.style.display = "none";
 
 function resizeGame() {
     var gameArea = document.getElementById('gameArea');
@@ -97,17 +45,91 @@ function resizeGame() {
 }
 resizeGame()
 
-var context = canvas.getContext('2d');
-socket.on('state', function(players) {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = 'green';
-  for (var id in players) {
-    var player = players[id];
-    context.beginPath();
-    context.arc(player.x*scale, player.y*scale, 10*scale, 0, 2 * Math.PI);
-    context.fill();
-  }
-});
+usernameInput.onkeydown = function(event) {
+    if (event.keyCode == 13) {
+        startGame(usernameInput.value);
+    }
+}
+
+function startGame(username){
+	//console.log(username);
+	usernameInput.parentNode.removeChild(usernameInput);
+
+	document.addEventListener('keydown', function(event) {
+	  switch (event.keyCode) {
+	    case 65: // A
+	      movement.left = true;
+	      break;
+	    case 87: // W
+	      movement.up = true;
+	      break;
+	    case 68: // D
+	      movement.right = true;
+	      break;
+	    case 83: // S
+	      movement.down = true;
+	      break;
+	  }
+	});
+
+	document.addEventListener('keyup', function(event) {
+	  switch (event.keyCode) {
+	    case 65: // A
+	      movement.left = false;
+	      break;
+	    case 87: // W
+	      movement.up = false;
+	      break;
+	    case 68: // D
+	      movement.right = false;
+	      break;
+	    case 83: // S
+	      movement.down = false;
+	      break;
+	  }
+	});
+
+	socket.emit('new player');
+
+	setInterval(function() {
+	  socket.emit('movement', movement);
+	}, 1000 / 60);
+
+	chat.style.display = "block";
+
+	function sendMessage(){
+		socket.emit('sendMessage',chatInput.value);
+		chatInput.value = "";
+	}
+
+	chatInput.onkeydown = function(event) {
+	    if (event.keyCode == 13) {
+	        sendMessage();
+	    }
+	}
+
+	socket.on ('loadMessage',function(data){
+		//console.log(data);
+		var para = document.createElement("p");
+		para.className = "message";
+		var text = document.createTextNode(data);
+		para.appendChild(text);
+		messages.prepend(para);
+	});
+
+	var context = canvas.getContext('2d');
+	socket.on('state', function(players) {
+	  context.clearRect(0, 0, canvas.width, canvas.height);
+	  context.fillStyle = 'green';
+	  for (var id in players) {
+	    var player = players[id];
+	    context.beginPath();
+	    context.arc(player.x*scale, player.y*scale, 10*scale, 0, 2 * Math.PI);
+	    context.fill();
+	  }
+	});
+}
+
 
 
 
