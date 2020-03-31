160199d5bd90a176910f29dd5dfbeab2ec448bd1
root <vladov3000@gmail.com>
2020-03-31 22:54:00 +0000

    docker fixed and working

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #port pass as arg is mapped to port 3000 inside the container
 
-docker run --rm -p ${1:-8080}:3000 engl320-tests
+docker run --rm -p ${1:-8000}:5000 -v ~/game1:/game1 game1
